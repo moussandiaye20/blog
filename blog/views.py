ef04bec87646854886b049eb5b750a372e31f153
ndiayemoussa <ndiayemoussa1960@gmail.com>
2021-12-01 18:09:53 +0100

    finished

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import  CreateView,UpdateView,DeleteView
 from .models import Post
 
 
@@ -11,3 +13,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): # new
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'body', 'author']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title',  'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
